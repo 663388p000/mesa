7c6f7d1d7f059774bf96840643745c9f59d64b56
Pipetto-crypto <pipetto-crypto@gmail.com>
2025-10-15 07:53:29 +0200

    wrapper_physical_device: disable VK_EXT_extended_dynamic_state and VK_EXT_extended_dynamic_state2 extensions on Mali

--- src/vulkan/wrapper/wrapper_physical_device.c
+++ src/vulkan/wrapper/wrapper_physical_device.c
@@ -238,6 +238,9 @@ VkResult enumerate_physical_device(struct vk_instance *_instance)
          if (strstr(engine_name, "DXVK")) {
             WRAPPER_LOG(info, "Forcing VK_EXT_robustness2");
             pdevice->vk.supported_extensions.EXT_robustness2 = true;
+            WRAPPER_LOG(info, "Disabling VK_EXT_extended_dynamic_state and VK_EXT_extended_dynamic_state2");
+            pdevice->vk.supported_extensions.EXT_extended_dynamic_state = false;
+            pdevice->vk.supported_extensions.EXT_extended_dynamic_state2 = false;
          }
          WRAPPER_LOG(info, "Disabling VK_EXT_calibrated_timestamps");
          pdevice->vk.supported_extensions.EXT_calibrated_timestamps = false;
