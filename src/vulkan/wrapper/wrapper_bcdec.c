27bbdcef8e25cb393e21dab17cb74610d492f5e0
Pipetto-crypto <pipetto-crypto@gmail.com>
2025-10-29 12:31:02 +0100

    wrapper_bcdec: add multithreaded BCn decompression

--- src/vulkan/wrapper/wrapper_bcdec.c
+++ src/vulkan/wrapper/wrapper_bcdec.c
@@ -1,11 +1,103 @@
 #include "wrapper_bcdec.h"
 #include "wrapper_log.h"
+
 #include <time.h>
+#include <pthread.h>
 
 #define BCDEC_BC4BC5_PRECISE
 #define BCDEC_IMPLEMENTATION
 
 #include "bcdec.h"
+
+struct decompression_params {
+   int block_x;
+   int block_y_count;
+   int block_y_start;
+   int stride;
+   int texel_size;
+   VkFormat format;
+   char *src;
+   char *dst;
+};
+
+static int
+get_block_size(VkFormat format) 
+{
+    switch(format) {
+       case VK_FORMAT_BC1_RGB_UNORM_BLOCK:
+       case VK_FORMAT_BC1_RGB_SRGB_BLOCK:
+       case VK_FORMAT_BC1_RGBA_SRGB_BLOCK:
+       case VK_FORMAT_BC1_RGBA_UNORM_BLOCK:
+       case VK_FORMAT_BC4_UNORM_BLOCK:
+       case VK_FORMAT_BC4_SNORM_BLOCK:
+          return 8;
+       default:
+          return 16;
+    }
+}
+
+static void *
+decompression_routine(void *args)
+{
+   struct decompression_params *params = args;
+
+   char *src = params->src;
+   char *dst_base = params->dst;
+   
+   for (int by = 0; by < params->block_y_count; by++) {
+      for (int bx = 0; bx < params->block_x; bx++) {
+         int pixel_x = (bx * 4);
+         int pixel_y = (by + params->block_y_start) * 4;
+         char *dst = dst_base + (pixel_y * params->stride) + (pixel_x * params->texel_size);
+         if (!dst || !src)
+            return NULL;
+            
+         switch (params->format) {
+            case VK_FORMAT_BC1_RGB_UNORM_BLOCK:
+            case VK_FORMAT_BC1_RGB_SRGB_BLOCK:
+            case VK_FORMAT_BC1_RGBA_SRGB_BLOCK:
+            case VK_FORMAT_BC1_RGBA_UNORM_BLOCK:
+               bcdec_bc1(src, dst, params->stride);
+               src += BCDEC_BC1_BLOCK_SIZE;
+               break;
+            case VK_FORMAT_BC2_SRGB_BLOCK:
+            case VK_FORMAT_BC2_UNORM_BLOCK:
+               bcdec_bc2(src, dst, params->stride);
+               src += BCDEC_BC2_BLOCK_SIZE;
+               break;
+            case VK_FORMAT_BC3_UNORM_BLOCK:
+            case VK_FORMAT_BC3_SRGB_BLOCK:
+               bcdec_bc3(src, dst, params->stride);
+               src += BCDEC_BC3_BLOCK_SIZE;
+               break;
+            case VK_FORMAT_BC4_UNORM_BLOCK:
+            case VK_FORMAT_BC4_SNORM_BLOCK:
+               bcdec_bc4(src, dst, params->stride, params->format == VK_FORMAT_BC4_SNORM_BLOCK);
+               src += BCDEC_BC4_BLOCK_SIZE;
+               break;
+            case VK_FORMAT_BC5_SNORM_BLOCK:
+            case VK_FORMAT_BC5_UNORM_BLOCK:
+               bcdec_bc5(src, dst, params->stride, params->format == VK_FORMAT_BC5_SNORM_BLOCK);
+               src += BCDEC_BC5_BLOCK_SIZE;
+               break;
+            case VK_FORMAT_BC6H_SFLOAT_BLOCK:
+            case VK_FORMAT_BC6H_UFLOAT_BLOCK:
+               bcdec_bc6h_half(src, dst, params->stride, params->format == VK_FORMAT_BC6H_SFLOAT_BLOCK);
+               src += BCDEC_BC6H_BLOCK_SIZE;
+               break;
+            case VK_FORMAT_BC7_SRGB_BLOCK:
+            case VK_FORMAT_BC7_UNORM_BLOCK:
+               bcdec_bc7(src, dst, params->stride);
+               src += BCDEC_BC7_BLOCK_SIZE;
+               break;
+            default:
+               break;
+         }
+      }
+   }
+   
+   return NULL;
+}
 
 VkFormat 
 get_format_for_bcn(VkFormat bcn_format)
@@ -99,74 +191,75 @@ decompress_bcn_format(void *srcBuffer,
 					  VkFormat format,
 					  int offset)
 {
-   char *src = srcBuffer + offset;
-   char *dst = dstBuffer;
-   clock_t start, end;
    static int wrapper_mark_bcn =  -1;
+   static int wrapper_no_bcn_thread = -1;
 
    if (wrapper_mark_bcn == -1)
       wrapper_mark_bcn = getenv("WRAPPER_MARK_BCN") && atoi(getenv("WRAPPER_MARK_BCN"));
 
+   if (wrapper_no_bcn_thread == -1)
+      wrapper_no_bcn_thread = getenv("WRAPPER_NO_BCN_THREAD") && atoi(getenv("WRAPPER_NO_BCN_THREAD"));
+
    int texel_size = get_texel_size_for_format(get_format_for_bcn(format));
+   int block_size = get_block_size(format);
+   int block_x = (w + 3) / 4;
+   int block_y = (h + 3) / 4;
+   int stride = w * texel_size;
+   char *src = srcBuffer + offset;
 
-   if (WRAPPER_LOG_LEVEL(info))
-      start = clock();
+   if (wrapper_no_bcn_thread) {
+      struct decompression_params args[1];
+      args[0].src = src;
+      args[0].dst = dstBuffer;
+      args[0].block_x = block_x;
+      args[0].format = format;
+      args[0].block_y_count = block_y;
+      args[0].block_y_start = 0;
+      args[0].stride = stride;
+      args[0].texel_size = texel_size;
+      decompression_routine(&args[0]);
+   } else {
+      int core_count = sysconf(_SC_NPROCESSORS_CONF);
+      int num_threads;
+      if (block_y >= core_count)
+         num_threads = core_count;
+      else if (block_y >= 4)
+         num_threads = 4;
+      else
+         num_threads = 1;
+      
+      int rows_per_thread = block_y / num_threads;
+      int rem = block_y % num_threads;
 
-   for (int i = 0; i < h; i += 4) {
-      for (int j = 0; j < w; j += 4) {
-         dst = dstBuffer + (i * w * texel_size) + j * texel_size;
-         switch (format) {
-            case VK_FORMAT_BC1_RGB_UNORM_BLOCK:
-            case VK_FORMAT_BC1_RGB_SRGB_BLOCK:                                           
-            case VK_FORMAT_BC1_RGBA_SRGB_BLOCK:                                          
-            case VK_FORMAT_BC1_RGBA_UNORM_BLOCK:
-               bcdec_bc1(src, dst, w * texel_size);
-               src += BCDEC_BC1_BLOCK_SIZE;
-               break;
-            case VK_FORMAT_BC2_SRGB_BLOCK:
-            case VK_FORMAT_BC2_UNORM_BLOCK:
-               bcdec_bc2(src, dst, w * texel_size);
-               src += BCDEC_BC2_BLOCK_SIZE;
-               break;
-            case VK_FORMAT_BC3_UNORM_BLOCK:
-            case VK_FORMAT_BC3_SRGB_BLOCK:
-               bcdec_bc3(src, dst, w * texel_size);
-               src += BCDEC_BC3_BLOCK_SIZE;
-               break;
-            case VK_FORMAT_BC4_UNORM_BLOCK:
-            case VK_FORMAT_BC4_SNORM_BLOCK:
-               bcdec_bc4(src, dst, w * texel_size, format == VK_FORMAT_BC4_SNORM_BLOCK);
-               src += BCDEC_BC4_BLOCK_SIZE;
-               break;
-            case VK_FORMAT_BC5_SNORM_BLOCK:                                              
-            case VK_FORMAT_BC5_UNORM_BLOCK:
-               bcdec_bc5(src, dst, w * texel_size, format == VK_FORMAT_BC5_SNORM_BLOCK);
-               src += BCDEC_BC5_BLOCK_SIZE;
-               break;
-            case VK_FORMAT_BC6H_SFLOAT_BLOCK:
-            case VK_FORMAT_BC6H_UFLOAT_BLOCK:
-               bcdec_bc6h_half(src, dst, w * texel_size, format == VK_FORMAT_BC6H_SFLOAT_BLOCK);
-               src += BCDEC_BC6H_BLOCK_SIZE;
-               break;
-            case VK_FORMAT_BC7_SRGB_BLOCK:                                               
-            case VK_FORMAT_BC7_UNORM_BLOCK:
-               bcdec_bc7(src, dst, w * texel_size);
-               src += BCDEC_BC7_BLOCK_SIZE;
-               break;
-            default:
-               break;
-         }
+      pthread_t *threads = malloc(sizeof(pthread_t) * num_threads);
+      struct decompression_params *args = malloc(sizeof(struct decompression_params) * num_threads);
+      int current_row = 0;
+
+      for (int i = 0; i < num_threads; i++) {
+         int rows = rows_per_thread + ((i < rem) ? 1 : 0);
+         args[i].src = src + (current_row * block_x * block_size);
+         args[i].dst = dstBuffer;
+         args[i].block_x = block_x;
+         args[i].format = format;
+         args[i].block_y_count = rows;
+         args[i].block_y_start = current_row;
+         args[i].stride = stride;
+         args[i].texel_size = texel_size;
+         pthread_create(&threads[i], NULL, decompression_routine, &args[i]);
+         current_row += rows;
       }
-   }
+   
+      for (int i = 0; i < num_threads; i++) {
+         pthread_join(threads[i], NULL);
+      }
 
-   if (WRAPPER_LOG_LEVEL(info)) {
-      end = clock();
-      double elapsed_time = (((double)(end - start)) / CLOCKS_PER_SEC) * 1000.0;
-      WRAPPER_LOG(info, "Raw texture data decompressed in %fms", elapsed_time);
+      free(threads);
+      free(args);
    }
-
+   
    if (wrapper_mark_bcn) {
       WRAPPER_LOG(info, "Marking BCn texture");
+      char *dst = dstBuffer;
       for (int i = 0; i < w; i++) {
          switch(format) {
             case VK_FORMAT_BC1_RGB_UNORM_BLOCK:
